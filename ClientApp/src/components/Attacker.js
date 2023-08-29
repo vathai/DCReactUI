934bacf517a80a2bd056b60860a99823e16ac122
vathai <campfiraga@gmail.com>
2023-08-29 10:13:49 -0700

    Add project files.

---
+++ ClientApp/src/components/Attacker.js
@@ -0,0 +1,12 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const CharStats = () => {
+
+    const [Attacker] = useState();
+
+    return (
+        <div>
+            (Attacker != null) ? <h3>{Attacker.Level}</h3>
+        </div>
+        )
+}
